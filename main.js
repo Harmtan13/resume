df8b46a2d953bb3cfbba245d6c3c8ffc1b75f864
Tanner Harman <harmtan2@gmail.com>
2019-12-30 13:35:28 -0700

    Finished Cover Letter

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 const dateInput = document.querySelector('.date');
 const monthNames = ["Jan", 
-"Feb", 
-"Mar", 
-"Apr", 
-"May", 
-"Jun",
-"Jul", 
-"Aug", 
-"Sept", 
-"Oct", 
-"Nov", 
-"Dec"
-];
+                    "Feb", 
+                    "Mar", 
+                    "Apr", 
+                    "May", 
+                    "Jun",
+                    "Jul", 
+                    "Aug", 
+                    "Sept", 
+                    "Oct", 
+                    "Nov", 
+                    "Dec" ];
 
 let date = new Date();
-
 dateInput.textContent = `${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+
+const formInput = document.querySelectorAll('.form-input');
+const formOutput = document.querySelectorAll('.output');
+
+formInput.forEach(input => {
+  input.addEventListener('input', function(e) {
+    formOutput.forEach(output => {
+     if (output.classList.value.includes(input.id)) {
+       output.textContent = input.value
+     }
+    })
+  });
+});
